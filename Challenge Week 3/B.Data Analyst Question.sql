bc19712348f33004e7feeaae231380e71e0437e2
Anh-Tho Tran <76476887+thota18411@users.noreply.github.com>
2021-08-28 21:58:08 +0700

    Create B.Data Analyst Question.sql

---
+++ Challenge Week 3/B.Data Analyst Question.sql
@@ -0,0 +1,22 @@
+--How many customers has Foodie-Fi ever had?
+--What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+--What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
+--What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
+--How many customers have churned straight after their initial free trial - what percentage is this rounded to the nearest whole number?
+--What is the number and percentage of customer plans after their initial free trial?
+--What is the customer count and percentage breakdown of all 5 plan_name values at 2020-12-31?
+--How many customers have upgraded to an annual plan in 2020?
+--How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
+--Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
+--How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
+
+--1
+SELECT COUNT(DISTINCT customer_id)
+FROM foodie_fi.subscriptions
+--2
+SELECT COUNT(DISTINCT customer_id) as number_of_cus,DATE_PART('month',start_date) As by_month
+FROM foodie_fi.subscriptions
+JOIN foodie_fi.plans
+USING (plan_id)
+WHERE plan_name='trial'
+GROUP BY DATE_PART('month',start_date)
